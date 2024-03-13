6f8d9890716bd49e1f24cf990b00f0cff7d998c9
Germar Bunda <germarbunda75@gmail.com>
2024-03-14 00:48:35 +0800

    Create basiFinalNani.ino

---
+++ basiFinalNani/basiFinalNani.ino
@@ -0,0 +1,50 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char WIFI_SSID[] = "PL2";
+const char WIFI_PASSWORD[] = "@PL22223";
+
+String HOST_NAME = "http://afas.atwebpages.com"; // change to your PC's IP address
+String PATH_NAME   = "/alert.php";
+String queryString = "?coordinates";
+String coordinates = "=uli%nata";
+
+void setup() {
+  Serial.begin(9600); 
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  
+  HTTPClient http;
+
+  http.begin(HOST_NAME + PATH_NAME + queryString + coordinates) ; //HTTP
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if(httpCode > 0) {
+    // file found at server
+    if(httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+}
+
+void loop() {
+
+}
